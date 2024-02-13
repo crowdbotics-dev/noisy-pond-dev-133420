8a51e1fc8a1c1bd0f0d17e1fb96da90c9762e03b
Crowdbotics <engineering@crowdbotics.com>
2024-02-13 23:18:06 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "noisy-pond-dev-133420",
+  url: "https://noisy-pond-dev-133420.botics.co",
+  api: "https://noisy-pond-dev-133420.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
